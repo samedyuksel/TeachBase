70d6824f5925c6690d48939553e4fbfc3266fd3d
samedyuksel <yukselsamed@hotmail.com>
2020-10-14 00:35:10 +0300

    first commit via Intellij Idea

--- TeacherBase/frontend/angularUi/backend/models/course.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose')
-
-var courseSchema = new mongoose.Schema({
-    name:String,
-    author:{type:mongoose.Schema.Types.ObjectId,ref:'Author'}
-})
-
-module.exports = mongoose.model('Course', courseSchema)
